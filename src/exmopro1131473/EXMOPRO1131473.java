b9a2000bb195c715ef904da6c47b41011a10f426
Aluno <Aluno@172.18.115.16>
2015-03-23 16:11:22 +0000

    Exercícios de MOPRO

--- src/exmopro1131473/EXMOPRO1131473.java
+++ src/exmopro1131473/EXMOPRO1131473.java
@@ -1,19 +1,54 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-package exmopro1131473;
 
-/**
- *
- * @author Aluno
- */
+package exmopro1131473;
+import java.util.GregorianCalendar;
+import java.util.Locale;
+import javax.swing.JOptionPane;
+
 public class EXMOPRO1131473 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        JOptionPane.showMessageDialog(null, "Goodbye, cruel world!");
+        Data.printLong();
     }
+}
+class Data {
+
+    static int ano = GregorianCalendar.getInstance().get(GregorianCalendar.YEAR);
+    static int dia = GregorianCalendar.getInstance().get(GregorianCalendar.DATE);
+    static int mes = GregorianCalendar.getInstance().get(GregorianCalendar.MONTH);
+    
+    static void printInvShort() {
+        JOptionPane.showMessageDialog(null,Data.ano + "/" + Data.mes + "/"+ Data.dia);
+    }
+    
+    static void printLong() {
+        JOptionPane.showMessageDialog(null,Data.dia + " de " + (GregorianCalendar.getInstance().getDisplayName(GregorianCalendar.MONTH, GregorianCalendar.LONG, Locale.getDefault())) +" de "+ Data.ano);
+    }
+    
+    static void ReadDate() {
+        
+        try{
+            dia = Integer.parseInt(JOptionPane.showInputDialog("Introduza o dia do mês:"));
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null,"Número inválido.");
+        } catch (NullPointerException e) {
+            return;
+        }
+        
+        try{
+            mes = Integer.parseInt(JOptionPane.showInputDialog("Introduza o mês:"));
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null,"Número inválido.");
+        } catch (NullPointerException e) {
+            return;
+        }
+        
+        try{
+            ano = Integer.parseInt(JOptionPane.showInputDialog("Introduza o ano:"));
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null,"Número inválido.");
+        } catch (NullPointerException e) {
+        }
+    }
+    
 }
